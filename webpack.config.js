e89cbf6684881931b3011be76b35563b34289ca9
Dimayo <44707838+Dimayo@users.noreply.github.com>
2019-03-13 04:16:20 +0600

    Added fonts

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,24 @@
 const path = require('path');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
 module.exports = {
     entry: [
         './src/index.js',
         './src/index.scss'
     ],
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true,
+            }),
+            new OptimizeCssAssetsPlugin({})
+        ]
+    },
     output: {
         path: path.resolve(__dirname, './docs/'),
         filename: './bundle.js',
@@ -15,10 +26,6 @@ module.exports = {
     },
     module: {
         rules: [{
-                test: /\.pug$/,
-                use: ['pug-loader']
-            },
-            {
                 test: /\.js$/,
                 include: path.resolve(__dirname, 'src/js'),
                 use: {
@@ -29,46 +36,52 @@ module.exports = {
                 }
             },
             {
-                test: /\.scss$/,
-                include: path.resolve(__dirname, 'src/scss'),
-                use: ExtractTextPlugin.extract({
-                    use: [{
-                            loader: "css-loader",
-                            options: {
-                                url: false
-                            }
-                        },
-                        {
-                            loader: "postcss-loader"
-                        },
-                        {
-                            loader: "sass-loader",
-                            options: {
-                                sourceMap: true
-                            }
-                        }
-                    ]
-                })
+                test: /\.pug$/,
+                use: ['pug-loader']
             },
+            {
+                test: /\.scss$/,
+                use: [{
+                        loader: "style-loader",
+                    }, {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+
+                    {
+                        loader: "css-loader",
+                        options: {
+                            url: false
+                        }
+                    },
+                    {
+                        loader: "sass-loader",
+                    },
+                ]
+
+            },
+             {
+                 test: /\.(png|jpg|gif|svg)$/,
+                 use: [{
+                     loader: 'file-loader',
+                     options: {
+                         outputPath: 'images',
+                          name: '[name].[ext]',
+                     },
+                 }, ],
+             },
         ]
     },
     plugins: [
-        new ExtractTextPlugin({
-            filename: './style.css',
-            allChunks: true,
+        new MiniCssExtractPlugin({
+            filename: './style.css'
         }),
-        new CopyWebpackPlugin([{
-            from: './src/images',
-            to: './images'
-        },
-        {
-            from: './src/fonts',
-            to: './fonts'
-        },
-    ]),
+        new CopyWebpackPlugin([
+            {
+                from: './src/fonts',
+                to: './fonts'
+            }
+        ]),
         new HtmlWebpackPlugin({
-            inject: false,
-            hash: true,
             template: './src/index.pug',
             filename: 'index.html'
         }),
