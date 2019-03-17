dc89d7fc2c574cb87b6b59f59c2a13075a7259d1
Dimayo <44707838+Dimayo@users.noreply.github.com>
2019-03-17 07:40:50 +0600

    Corrected Education and Experience block using array

--- docs/bundle.js
+++ docs/bundle.js
@@ -91,9 +91,9 @@
   !*** ./node_modules/mini-css-extract-plugin/dist/loader.js!./node_modules/css-loader/dist/cjs.js??ref--6-2!./node_modules/sass-loader/lib/loader.js!./src/index.scss ***!
   \***********************************************************************************************************************************************************************/
 /*! no static exports found */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-eval("throw new Error(\"Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/lib/loader.js):\\n\\r\\n@import './components/education/experience.scss';\\r\\n^\\r\\n      File to import not found or unreadable: ./components/education/experience.scss.\\r\\n      in C:\\\\Code\\\\GitHub\\\\fsd_cv_page\\\\src\\\\index.scss (line 32, column 1)\\n    at runLoaders (C:\\\\Code\\\\GitHub\\\\fsd_cv_page\\\\node_modules\\\\webpack\\\\lib\\\\NormalModule.js:301:20)\\n    at C:\\\\Code\\\\GitHub\\\\fsd_cv_page\\\\node_modules\\\\loader-runner\\\\lib\\\\LoaderRunner.js:367:11\\n    at C:\\\\Code\\\\GitHub\\\\fsd_cv_page\\\\node_modules\\\\loader-runner\\\\lib\\\\LoaderRunner.js:233:18\\n    at context.callback (C:\\\\Code\\\\GitHub\\\\fsd_cv_page\\\\node_modules\\\\loader-runner\\\\lib\\\\LoaderRunner.js:111:13)\\n    at Object.render [as callback] (C:\\\\Code\\\\GitHub\\\\fsd_cv_page\\\\node_modules\\\\sass-loader\\\\lib\\\\loader.js:52:13)\\n    at Object.done [as callback] (C:\\\\Code\\\\GitHub\\\\fsd_cv_page\\\\node_modules\\\\neo-async\\\\async.js:8077:18)\\n    at options.error (C:\\\\Code\\\\GitHub\\\\fsd_cv_page\\\\node_modules\\\\node-sass\\\\lib\\\\index.js:294:32)\");\n\n//# sourceURL=webpack:///./src/index.scss?./node_modules/mini-css-extract-plugin/dist/loader.js!./node_modules/css-loader/dist/cjs.js??ref--6-2!./node_modules/sass-loader/lib/loader.js");
+eval("// extracted by mini-css-extract-plugin\n\n//# sourceURL=webpack:///./src/index.scss?./node_modules/mini-css-extract-plugin/dist/loader.js!./node_modules/css-loader/dist/cjs.js??ref--6-2!./node_modules/sass-loader/lib/loader.js");
 
 /***/ }),
 
